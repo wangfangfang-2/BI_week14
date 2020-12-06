b3944c90fc8d2e9b6f20225b5818e88e3ed0b981
Zhang Yuxi <zhangyuxi@ZhangdeMacBook-Pro.local>
2020-12-06 23:44:02 +0800

    second commit

--- load_new.py
+++ load_new.py
@@ -66,36 +66,12 @@ for index,row in df.iterrows():
     #print(longitude,latitude)
 get_location('五道口站','北京')
 df.to_csv('./subway.csv',index=False)
-import pandas as pd 
-import requests
-import re
-#计算两点之间的距离
-def compute_distance(longitude1,latitude1,longitude2,latitude2):
-    header={'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36'}
-    #requests_url='http://restapi.amap.com/v3/place/text?key=35fc3e2f362fa1eeec425f4468f2452b&keywords='+\keywords+'&types=&city'+city+'&children=1&offset=1&page=1&extensions=all'
-    #requests_url='http://restapi.amap.com/v3/distance?key=35fc3e2f362fa1eeec425f4468f2452b&origins=116.337581,39.993138&destination=116.339941,39.976228&type=1'
-    #requests_url='http://restapi.amap.com/v3/place/text?key=cb6271e8a1f293ded5422d57fbd0fcdc&keywords=五道口&types=&city=北京&children=1&offset=1&page=1&extensions=all'
-    #requests_url='http://restapi.amap.com/v3/place/text?key=cb6271e8a1f293ded5422d57fbd0fcdc&keywords='+keyword+'&types=&city='+city+'&children=1&offset=1&page=1&extensions=all'
-    requests_url='http://restapi.amap.com/v3/place/text?key=cb6271e8a1f293ded5422d57fbd0fcdc&keywords='+keyword+'&types=&city='+city+'&children=1&offset=1&page=1&extensions=all'
-    data=requests.get(requests_url,headers=header)
-    data.encoding='utf-8'
-    data=data.text
-    #print(data)
-    
-   #.*具有贪婪的性质，首先匹配到不能匹配为止；。*？则相反，一个匹配以后，就可以继续后面到
-    pattern='distance":"(.*?)","duration":"(.*?)"'
-   
-   #获取经纬度
-    result=re.findall(pattern,data)
-    
-    #return result
-    print(result[0][0],result[0][1])
-compute_distance(116.337742,39.992894,116.365023,39.812904)
+"""
 
 import pandas as pd 
 import requests
 import re
-
+"""
 #计算两点之间的距离
 def compute_distance(longitude1,latitude1,longitude2,latitude2):
     header={'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36'}
@@ -110,16 +86,18 @@ def compute_distance(longitude1,latitude1,longitude2,latitude2):
    
    #获取经纬度
     result=re.findall(pattern,data)
-    
-    return result
+    #result= pd.DataFrame.dropna(result,axis=0,how='any',inplace=True)
+    return result[0][0]
+    #print(len(result))
     #print(result[0][0],result[0][1])
 #compute_distance(116.337742,39.992894,116.365023,39.812904)
-compute_distance(116.337581,39.993138,116.339941,39.976228)
+#compute_distance(116.337581,39.993138,116.339941,39.976228)
 #数据加载
-data=pd.read_csv('./subway.csv')
-print(data)
+
+data=pd.read_csv('./subway1.csv')
+#print(data)
 from collections import defaultdict
-#保存图中两点之间的距离
+#保存图中两点之间的距离，graph为邻接矩阵表
 graph= defaultdict(dict)
 for i in range(data.shape[0]):
     site1=data.iloc[i]['site']
@@ -135,37 +113,37 @@ for i in range(data.shape[0]):
             distance = compute_distance(longitude1,latitude1,longitude2,latitude2)
             graph[name1][name2]=distance
             graph[name2][name1]=distance
-            print(name1,name2,distance)
+            #print(name1,name2,distance)
 import pickle
-output=open('graph.pkl','wb')
+output=open('graph1.pkl','wb')
 pickle.dump(graph,output)
 #获得指定城市的地铁路线
 
 import requests
 def get_page_content(request_url):
-    header={header={'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36'}}
-    html = requests.get(request_urt,headers=header,timeout=10)
+    header={'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36'}
+    html = requests.get(request_url,headers=header,timeout=10)
 requests_url = 'https://ditie.mapbar.com/beijing_line/'
 soup = get_page_content(requests_url)
 """
 
 import pickle
-file = open('graph.pkl','rb')
+file = open('graph1.pkl','rb')
 graph = pickle.load(file)
 #print(graph)
 
 #找到开销最小的节点
 def find_lowest_cost_node(costs):
     #查找最小节点，采用打擂法
-    lowest_costs=float('inf')
+    lowest_cost=float('inf')
     lowest_cost_node = None
     #做一个遍历
     for node in costs :
         #如果该节点没有被处理
         if not node in processed:
             #如果当前节点的开销比已经存在的开销小，那么更新该节点为开销最小的节点
-            if costs[node]<lowest_costs:
-                lowest_costs = costs[node]
+            if costs[node]<lowest_cost:
+                lowest_cost = costs[node]
                 lowest_cost_node = node 
     return lowest_cost_node
     #找到最短路径
@@ -184,6 +162,8 @@ def find_shortest_path():
 def dijkstra():
     #查询到目前开销最小的节点
     node = find_lowest_cost_node(costs)
+    print('当前cost最小节点',node)
+   
     #print('当前cost最小节点：',node)
     #使用找到的开销最小节点，计算它的邻居，是否可以通过它进行更新
     #如果所有节点都在processed,就结束
@@ -207,19 +187,22 @@ def dijkstra():
             #将当前节点标记为已处理
         processed.append(node)
         #下一步，继续找U中最短距离的节点，cost=U,processed=5
-        node = find_lowest_cost_node(costs)
-       # print('当前cost最小节点',node)
+        node=find_lowest_cost_node(costs)
 #循环完说明所有节点已经处理完，找最短路径
     shortest_path = find_shortest_path()
     shortest_path.reverse()
     print('从{}到{}的最短路径:{}'.format(start,end,shortest_path))
     #print('最短路径',shortest_path)
-    
-#def find_shortest_path():
-#    node=
+start = '回龙观站'
+end = '天坛东门站'  
+""" 
+start = '首经贸站'
+end = '五道口站'
 
-#start = '雍和宫站'
-#end = '西二旗站'
+start = '雍和宫站'
+end = '西二旗站'
+
+
 
 #start = '首经贸站'
 #end = '五道口站'
@@ -227,6 +210,7 @@ def dijkstra():
 
 start = '回龙观站'
 end = '天坛东门站'
+"""
 # 创建节点的开销表，cost是指从start到该节点的距离
 costs = {}
 # 存储父节点的Hash表，用于记录路径
@@ -247,6 +231,7 @@ costs[end]= float('inf')
 #
 #记录处理过的节点list
 """
+
 if __name__ == "__main__":
     site1 = '五道口站'
     site2 = '北京南站'
